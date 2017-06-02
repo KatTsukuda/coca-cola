8ac534dc85240d4b932d48275887062f10e066f5
Kat Tsukuda <BearHeartsBat@Kats-MacBook-Pro.local>
2017-06-02 16:51:36 -0700

    setup server.js to require express and establish server

---
+++ server.js
@@ -0,0 +1,24 @@
+// require express and other modules
+var express = require('express'),
+    app = express();
+
+
+
+//***DATABASE***//
+
+
+//***ROUTES***//
+
+
+//***HTML ENDPOINTS***//
+
+
+//***JSON ENDPOINTS***//
+
+
+//***SERVER***//
+
+//CONNECT TO LOCAL SERVER AND HEROKU
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express server on a rampage at http://localhost:3000/');
+})
